2f3273ca9e551d8d3e24c3f72a4fd074aa77da04
calvince <calmosh1421@gmail.com>
2019-07-30 15:34:27 +0300

    This will will add and implement constant variable used in query services

---
+++ app/src/main/java/com/example/events/Constants.java
@@ -0,0 +1,7 @@
+package com.example.events;
+
+public class Constants {
+    public static final String EVENT_TOKEN = BuildConfig.EVENT_TOKEN;
+    public static final String EVENT_BASE_URL = " https://www.eventbriteapi.com/v3/events/search";
+    public static final String EVENT_LOCATION_QUERY_PARAMETER = "location.address";
+}
