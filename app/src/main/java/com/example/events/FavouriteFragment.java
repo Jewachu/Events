98b4e039a8818b270340a5e346db66ae17b85473
calvince <calmosh1421@gmail.com>
2019-07-31 07:35:34 +0300

    This will add all favourite events to the db

--- app/src/main/java/com/example/events/FavouriteFragment.java
+++ app/src/main/java/com/example/events/FavouriteFragment.java
@@ -13,38 +13,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.util.zip.Inflater;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class FavouriteFragment extends Fragment{
-    @BindView(R.id.textView5) TextView txtEvent;
-    @BindView(R.id.textView6) TextView txtDate;
-    @BindView(R.id.textView7) TextView txtVenue;
-//    @BindView(R.id.lst_view) ListView  thisList;
+
     private static final String TAG = FavouriteFragment.class.getSimpleName();
-//    private String[] events ;
-//    private String[] dates ;
-//    private String[] venue ;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Favourite");
         View v =inflater.inflate(R.layout.fragment_favourite,container,false);
-//        ButterKnife.bind(this,v);
-
-//        EventAdapter adapter = new EventAdapter(getActivity(),android.R.layout.simple_list_item_1,events,dates,venue);
-//        thisList.setAdapter(adapter);
         return v;
     }
 
-//    @Override
-//    public void applyTexts(String event, String date, String venue) {
-//        txtEvent.setText(event);
-//        txtDate.setText(date);
-//        txtVenue.setText(venue);
-//
-//    }
+
 }
