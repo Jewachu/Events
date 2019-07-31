9008618a271018c1b836f1282dabf0de92d18fc3
calvince <calmosh1421@gmail.com>
2019-07-31 07:35:11 +0300

    This will implement the search function for the events based on location

--- app/src/main/java/com/example/events/HomeFragment.java
+++ app/src/main/java/com/example/events/HomeFragment.java
@@ -4,6 +4,7 @@ import android.app.LauncherActivity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,28 +25,36 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class HomeFragment extends Fragment {
-    @BindView(R.id.txt_ename) TextView txtText1;
-    @BindView(R.id.btn_one) Button button;
+    @BindView(R.id.txt_ename)
+    TextView txtText1;
+    @BindView(R.id.btn_one)
+    Button button;
+    @BindView(R.id.edtLocation)
+    EditText mLocationEditText;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Home");
-        View thisHome = inflater.inflate(R.layout.fragment_home,container,false);
-        ButterKnife.bind(this,thisHome);
+        View thisHome = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, thisHome);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openDialog();
-                Toast.makeText(getActivity(),"Book event",Toast.LENGTH_SHORT).show();
+                String location = mLocationEditText.getText().toString();
+                if (TextUtils.isEmpty(mLocationEditText.getText())){
+                    mLocationEditText.setError("Input field");
+                    Toast.makeText(getActivity(),"Please input this field",Toast.LENGTH_SHORT).show();
+                }else {
+                    FragmentManager manager = getFragmentManager();
+                    SearchFragment searchFragment = SearchFragment.newFragmentInstance(location);
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, searchFragment, "Details")
+                            .addToBackStack(null).commit();
+                }
             }
         });
-        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(),"fonts/FFF_Tusj.ttf");
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/FFF_Tusj.ttf");
         txtText1.setTypeface(typeface);
         return thisHome;
-    }
-    private void openDialog(){
-        DetailDialog detailDialog = new DetailDialog();
-        detailDialog.show(getFragmentManager(),"Input Detail");
     }
 }
