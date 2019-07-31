19cbfd64f6ec3725f9b12ae07fe3a9ef9343e54f
calvince <calmosh1421@gmail.com>
2019-07-31 07:37:39 +0300

    This will will delete TextView and also implement viewPager

--- app/src/main/java/com/example/events/AllDetail.java
+++ app/src/main/java/com/example/events/AllDetail.java
@@ -3,22 +3,30 @@ package com.example.events;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.events.adapters.EventPageAdapter;
+import com.example.events.models.Event;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.View;
 import android.widget.TextView;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AllDetail extends AppCompatActivity {
-    @BindView(R.id.textView5) TextView txtEvent;
-
+    @BindView(R.id.viewPager) ViewPager mViewPager;
+    private EventPageAdapter eventPageAdapter;
+    ArrayList<Event> mEvents = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +38,13 @@ public class AllDetail extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
         ButterKnife.bind(this);
+        mEvents = Parcels.unwrap(getIntent().getParcelableExtra("events"));
+        int startingPosition = getIntent().getIntExtra("position",0);
+        eventPageAdapter = new EventPageAdapter(getSupportFragmentManager(),mEvents);
+        mViewPager.setAdapter(eventPageAdapter);
+        mViewPager.setCurrentItem(startingPosition);
 
-        Intent intent = getIntent();
-      String event= intent.getStringExtra("event");
-      String date = intent.getStringExtra("date");
-      String venue = intent.getStringExtra("venue");
-       txtEvent.setText(event);
+
 
 
 
