954f6982020d7d234ae72920f6890b3010964dee
calvince <calmosh1421@gmail.com>
2019-10-03 13:56:10 +0300

    This commit will implement changes from AboutActivity

--- app/src/main/java/com/example/events/ui/AboutActivity.java
+++ app/src/main/java/com/example/events/ui/AboutActivity.java
@@ -1,7 +1,8 @@
-package com.example.events;
+package com.example.events.ui;
 
 import android.os.Bundle;
 
+import com.example.events.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
